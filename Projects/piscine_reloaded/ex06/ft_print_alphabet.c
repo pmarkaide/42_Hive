8bfb74fe74356110db5a22f3ad79df185b9892a1
Pablo Markaide <pablo.markaide@gmail.com>
2023-10-02 17:31:49 +0300

    feat: add ex00 to ex06

---
+++ Projects/piscine_reloaded/ex06/ft_print_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: peitx <peitx@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/02 16:26:22 by peitx             #+#    #+#             */
+/*   Updated: 2023/10/02 16:44:35 by peitx            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	char	letter;
+
+	letter = 'a';
+	while (letter <= 'z')
+	{
+		write(1, &letter, 1);
+		letter++;
+	}
+}
+
+void main()
+{
+	ft_print_alphabet();
+}
